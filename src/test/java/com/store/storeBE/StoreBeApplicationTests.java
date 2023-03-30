4efe1e4d816f3b464f5cea8e4ac45485c5b9d616
gsheshe <gsheshe13@gmail.com>
2023-03-30 16:08:30 +0200

    Initial commit

---
+++ src/test/java/com/store/storeBE/StoreBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.storeBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
